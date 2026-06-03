5999bbba2508f8c208942c03f10c63ec69af71c8
stepan41k <russia.zont@gmail.com>
2026-06-03 16:27:36 +0300

    feat(events): add username to message created event

--- pkg/events/message_event.go
+++ pkg/events/message_event.go
@@ -10,6 +10,7 @@ type MessageCreatedEvent struct {
 	MessageID uuid.UUID  `json:"message_id"`
 	ChannelID uuid.UUID  `json:"channel_id"`
 	AuthorID  uuid.UUID  `json:"author_id"`
+	Username   string     `json:"username"`
 	Content   string     `json:"content"`
 	ReplyTo   *uuid.UUID `json:"reply_to,omitempty"`
 	CreatedAt time.Time  `json:"created_at"`
